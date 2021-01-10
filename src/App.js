d0526173c8c03fa4888aa93781bbe7dafa55a8d0
arbabali1441 <arbabali1441@gmail.com>
2021-01-11 04:52:39 +0500

    Add webcam functionality 37:47

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './WebcamCapture';
 
 function App() {
   return (
     <div className="app">
       <h1>Lets Build Snapchat Clone with Arbab Ali</h1>
+        <WebcamCapture />
     </div>
   );
 }
