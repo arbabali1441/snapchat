126fe9c532fbde62d80424129441e14945c2c794
arbabali1441 <arbabali1441@gmail.com>
2021-01-11 07:21:29 +0500

    Add preview page button functionality 1:04:19

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
 import {  BrowserRouter as Router, Switch, Route  } from "react-router-dom";
+import Preview from './Preview';
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
       <Router>
         <div className="app__body">
           <Switch>
-          <Route exect path="/test">
-            <h1>Yoo What's up?</h1>
+          <Route exect path="/preview">
+            <Preview />
             </Route>
             <Route exect path="/">
             <WebcamCapture />
