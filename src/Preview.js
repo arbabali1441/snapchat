2d28ad848ebb12f6bd9841d0cdd44cd8981eb335
arbabali1441 <arbabali1441@gmail.com>
2021-01-14 06:26:56 +0500

    Add Preview icons 1:26:22

--- src/Preview.js
+++ src/Preview.js
@@ -4,6 +4,13 @@ import { useHistory } from 'react-router-dom';
 import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import "./Preview.css";
 import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -18,14 +25,23 @@ function Preview() {
 
   const closePreview = () => {
     dispatch(resetCameraImage());
-  }
+  };
 
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt=""/>
     </div>
-  )
+  );
 }
 
 export default Preview;
